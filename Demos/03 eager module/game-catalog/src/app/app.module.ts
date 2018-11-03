116568ae95220937605246bc0b69d548171d3a89
Jaime Salas Zancada <szjaime@gmail.com>
2018-11-03 11:59:02 +0100

    routing module demo added

---
+++ Demos/03 eager module/game-catalog/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { MenuComponent } from './home/menu.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { ShellComponent } from './home/shell.component';
+import { PageNotFoundComponent } from './home/page-not-found.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    WelcomeComponent,
+    ShellComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Demos/03 eager module/game-catalog/src/app/home/page-not-found.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-not-found',
+  template: `
+    <h1>Lose yourself...</h1>
+  `,
+  styles: []
+})
+export class PageNotFoundComponent {}
