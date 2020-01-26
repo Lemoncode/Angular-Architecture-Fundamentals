e8b4691d97fff6bb676d61ebc43b26c6d5d2a0bf
jaimesalas <jaime.salas@lemoncode.net>
2020-01-26 18:56:10 +0100

    added container component

---
+++ angular 8/Demos/26 container components/game-catalog/src/app/video-consoles/components/video-console-list/video-console-list.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+
+// import { VideoConsoleModel } from '../video-console.model';
+
+import { Observable } from 'rxjs';
+
+import { Store, select } from '@ngrx/store';
+
+// import * as fromVideoConsole from '../state/video-consoles.reducer';
+// import * as videoConsolesActions from '../state/video-consoles.actions';
+
+@Component({
+  selector: 'app-video-console-list',
+  templateUrl: './video-console-list.component.html',
+  styleUrls: ['./video-console-list.component.css']
+})
+export class VideoConsoleListComponent {
+  // errorMessage$: Observable<string>;
+  // displayCode: boolean;
+  // videoConsoles$: Observable<VideoConsoleModel[]>;
+  // selectedVideoConsole: VideoConsoleModel | null;
+
+  // constructor(
+  //   private store: Store<fromVideoConsole.State>
+  //   ) { }
+
+  // ngOnInit(): void {
+  //   this.store.pipe(
+  //     select(fromVideoConsole.getCurrentVideoConsole)
+  //   ).subscribe(
+  //     selectedVideoConsole => this.selectedVideoConsole = selectedVideoConsole
+  //   )
+
+  //   this.store.dispatch(new videoConsolesActions.Load());
+  //   this.videoConsoles$ = this.store.pipe(
+  //     select(fromVideoConsole.getVideoConsoles)
+  //   );
+  //   this.errorMessage$ = this.store.pipe(
+  //     select(fromVideoConsole.getError)
+  //   );
+
+  //   this.store.pipe(
+  //     select(fromVideoConsole.getShowVideoConsoleCode)
+  //   ).subscribe(
+  //     showVideoConsoleCode => this.displayCode = showVideoConsoleCode,
+  //   );
+  // }
+
+  // checkChanged(value: boolean): void {
+  //   this.store.dispatch(new videoConsolesActions.ToggleVideoConsoleCode(value));
+  // }
+
+  // newVideoConsole(): void {
+  //   this.store.dispatch(new videoConsolesActions.InitializeCurrentVideoConsole());
+  // }
+
+  // videoConsoleSelected(videoConsole: VideoConsoleModel) : void {
+  //   this.store.dispatch(new videoConsolesActions.SetCurrentVideoConsole(videoConsole));
+  // }
+}
---
+++ angular 8/Demos/26 container components/game-catalog/src/app/video-consoles/containers/video-console-board/video-console-board.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { VideoConsoleModel } from '../../video-console.model';
+import { Store, select } from '@ngrx/store';
+import * as fromVideoConsole from '../../state/video-consoles.reducer';
+import * as videoConsolesActions from '../../state/video-consoles.actions';
+
+@Component({
+  selector: 'app-video-console-board',
+  templateUrl: './video-console-board.component.html',
+  styleUrls: ['./video-console-board.component.css']
+})
+export class VideoConsoleBoardComponent implements OnInit {
+  errorMessage$: Observable<string>;
+  displayCode$: Observable<boolean>;
+  videoConsoles$: Observable<VideoConsoleModel[]>;
+  selectedVideoConsole$: Observable<VideoConsoleModel>;
+
+  constructor(
+    private store: Store<fromVideoConsole.State>
+    ) { }
+
+  ngOnInit(): void {
+    this.selectedVideoConsole$ = this.store.pipe(
+      select(fromVideoConsole.getCurrentVideoConsole)
+    );
+
+    this.store.dispatch(new videoConsolesActions.Load());
+    this.videoConsoles$ = this.store.pipe(
+      select(fromVideoConsole.getVideoConsoles)
+    );
+    this.errorMessage$ = this.store.pipe(
+      select(fromVideoConsole.getError)
+    );
+
+    this.displayCode$ = this.store.pipe(
+      select(fromVideoConsole.getShowVideoConsoleCode)
+    );
+  }
+
+  checkChanged(value: boolean): void {
+    this.store.dispatch(new videoConsolesActions.ToggleVideoConsoleCode(value));
+  }
+
+  newVideoConsole(): void {
+    this.store.dispatch(new videoConsolesActions.InitializeCurrentVideoConsole());
+  }
+
+  videoConsoleSelected(videoConsole: VideoConsoleModel) : void {
+    this.store.dispatch(new videoConsolesActions.SetCurrentVideoConsole(videoConsole));
+  }
+
+}
