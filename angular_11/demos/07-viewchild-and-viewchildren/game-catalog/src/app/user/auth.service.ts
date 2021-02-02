6a92f451f22f21599b2de4d9764b58ce7d5d5a7b
Jaime Salas Zancada <jaimesalaszancada@Jaimes-MacBook-Pro.local>
2021-02-03 00:58:29 +0100

    added viewchild and viewchildren demos

---
+++ angular_11/demos/07-viewchild-and-viewchildren/game-catalog/src/app/user/auth.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { UserModel } from './user.model';
+
+@Injectable()
+export class AuthService {
+  currentUser: UserModel | null;
+  redirectUrl: string;
+
+  constructor() { }
+
+  isLoggedIn(): boolean {
+    return !!this.currentUser;
+  }
+
+  login(userName: string, password: string): void {
+    // TODO: Implement backend service
+    this.currentUser = {
+      id: 34,
+      isAdmin: false,
+      userName: userName,
+    };
+  }
+
+  logout(): void {
+    this.currentUser = null;
+  }
+}
---
+++ angular_11/demos/07-viewchild-and-viewchildren/game-catalog/src/app/user/login.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { AuthService } from './auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  errorMessage: string;
+  pageTitle = 'Log In';
+
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  cancel(): void {
+    this.router.navigate(['welcome']);
+  }
+
+  login(loginForm: NgForm): void {
+    if (loginForm && loginForm.valid) {
+      const { userName, password } = loginForm.form.value;
+      this.authService.login(userName, password);
+
+      if (this.authService.redirectUrl) {
+        this.router.navigateByUrl(this.authService.redirectUrl);
+      } else {
+        // TODO: Navigate to games
+      }
+    }
+  }
+}
