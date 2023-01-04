f38e0ea56e43d79ae5ae9aaeab9c977a64702562
Jaime Salas Zancada <jaimesalaszancada@Jaimes-MacBook-Pro.local>
2023-01-04 16:31:13 +0100

    started child components demo

---
+++ angular_13/demos/08-child-components/game-catalog/src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { GameData } from './games/game-data';
+
+import { AppComponent } from './app.component';
+import { MenuComponent } from './home/menu.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { ShellComponent } from './home/shell.component';
+import { PageNotFoundComponent } from './home/page-not-found.component';
+
+import { CoreModule } from './core/core.module';
+
+import { UserModule } from './user/user.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    WelcomeComponent,
+    ShellComponent,
+    PageNotFoundComponent,
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(GameData),
+    CoreModule,
+    UserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ angular_13/demos/08-child-components/game-catalog/src/app/home/menu.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styles: [
+    `
+      .nav-full-width {
+        width: 100%;
+      }
+
+      .nav-end {
+        margin-left: auto;
+      }
+    `,
+  ],
+})
+export class MenuComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
