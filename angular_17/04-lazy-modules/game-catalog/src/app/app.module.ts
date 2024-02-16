17f7777dea7c50ce1109a870c0914f45631c0114
Jaime Salas Zancada <jaimesalaszancada@Jaimes-MacBook-Pro.local>
2024-02-16 21:31:19 +0100

    updating demos angular 17

---
+++ angular_17/04-lazy-modules/game-catalog/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { MenuComponent } from './home/menu.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { ShellComponent } from './home/shell.component';
+import { PageNotFoundComponent } from './home/page-not-found.component';
+
+import { AppRoutingModule } from './app-routing.module';
+import { UserModule } from './user/user.module';
+
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { GameData } from './games/game-data';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    WelcomeComponent,
+    ShellComponent,
+    PageNotFoundComponent,
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(GameData),
+    UserModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
